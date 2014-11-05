cb10dd4282c82546d8c07c6f33f3b08400620b07
Damien Zufferey <zufferey@csail.mit.edu>
2014-11-05 14:54:34 -0500

    better handling of $ in Namer

--- src/main/scala/dzufferey/utils/Namer.scala
+++ src/main/scala/dzufferey/utils/Namer.scala
@@ -9,11 +9,30 @@ class Namer {
   private def counter = new java.util.concurrent.atomic.AtomicInteger()
 
   def apply(prefix: String, preserve: Boolean = false): String = {
-    val realPrefix = if (preserve) prefix else prefix.replaceFirst("\\$\\d*\\z", "")
+    val (realPrefix, current) =
+      if (preserve) {
+        (prefix, 0)
+      } else {
+        val idx = prefix.lastIndexOf("$")
+        if (idx == -1) {
+          (prefix, 0)
+        } else {
+          try {
+            (prefix.substring(0, idx), prefix.substring(idx+1).toInt)
+          } catch {
+            case e: java.lang.NumberFormatException =>
+              (prefix, 0)
+          }
+        }
+      }
     val c1 = counter
-    val c2 = map.putIfAbsent(prefix, c1)
+    val c2 = map.putIfAbsent(realPrefix, c1)
     val c3 = if (c2 == null) c1 else c2
-    realPrefix.trim + "$" + c3.incrementAndGet
+    var v = c3.incrementAndGet
+    while (v <= current) {
+      v = c3.incrementAndGet
+    }
+    realPrefix.trim + "$" + v
   }
 
 }
